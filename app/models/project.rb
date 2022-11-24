81a7d81e74b4496ffd56721a38c5c94258377ee2
DerekGary <jgary4151@gmail.com>
2022-11-24 00:43:42 +0000

    Create active record validation and RSpec test for description

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
+=begin
+Active Record Validation
+    Purpose: Ensures that listed attributes
+    will be a part of the Project class by marking it
+    as invalid when given erroneous test data by RSpec.
+=end
+
 class Project < ApplicationRecord
-    validates :title, presence: true
     validates :description, presence: true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,20 +1,23 @@
 require "rails_helper"
 
+=begin
+RSpec Testing
+    Purpose: Tests the behavior of objects within the model to drive development.
+    Works with active record validation to ensure objects are working appropriately
+    and as intended. Below, I added in a line which shows that description will not
+    take 'nil' as input.
+=end
 RSpec.describe Project, type: :model do
         context "validations tests" do
-            it "ensures the title is present" do
-                project = Project.new(description: "Content of the description")
-                expect(project.valid?).to eq(false)
-            end
-            
             it "ensures the description is present" do
-                project = Project.new(description: "Content of the description")
-                expect(project.valid?).to eq(false)
+                project = Project.new(description: "Test Description")
+                expect(project.valid?).to eq(true)
             end
 
-            it "should be able to save project" do
-                project = Project.new(title: "Title", description: "Some description content goes here")
-                expect(project.save).to eq(true)
+            # Personally added this line for my own research.
+            it "ensures the description cannot take nil" do
+                project = Project.new(description: nil)
+                expect(project.valid?).to eq(false)
             end
         end
 
