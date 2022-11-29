a07ba09b72d45bf973858d971ee7c5d2e3749009
DerekGary <jgary4151@gmail.com>
2022-11-29 03:16:42 +0000

    Reroute users to different views depending on their authentication status

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   #root "projects#index"
-  root to: "home#index"
-  resources :projects
+  authenticated do
+    root "projects#index", as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "home#index"
+    resources :projects
+  end
 end
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
