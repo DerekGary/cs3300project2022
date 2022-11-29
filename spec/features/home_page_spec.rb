8b70753a440849ffdc0d2317bc1f68642cc62f8b
DerekGary <jgary4151@gmail.com>
2022-11-29 23:49:38 +0000

    Rework front-end for 'New Project' page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,15 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Visiting the HomePage", type: :feature do
-  context "user should be rerouted when navbar link is clicked" do
-    before (:each) do
-      visit root_path
-    end
-    scenario "the visitor should be routed to homepage when 'Home' link is clicked" do
-      click_button "Home"
-      expect(page).to have_link("Sign In")
-    end
-  end
   scenario "The visitor should see projects" do
     visit root_path
     expect(page).to have_text("Projects")
