7054fdd9ccb7129df817817e8c57461e4aec5d34
DerekGary <jgary4151@gmail.com>
2022-11-29 02:53:11 +0000

    Replace empty href tags with Devise routes for navbar user auth links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :projects
 end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
